59d74f273a5808e54aec26da6f21331f9e2dc7a5
arpit-raj04 <154455449+arpit-raj04@users.noreply.github.com>
2025-06-06 14:56:14 +0530

    Integrated LangSmith & created Language Translator

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_openai import AzureChatOpenAI
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()  # Load env variables from .env
 
@@ -11,5 +12,11 @@ model = AzureChatOpenAI(
     api_key=os.environ["AZURE_OPENAI_API_KEY"]
 )
 
-response = model.invoke("Hello, world!")
+# response = model.invoke("Hello, world!")
+# print(response.content)
+
+messages = [    SystemMessage(content = "Translate the following from English into Russian"),
+    HumanMessage(content = "Hello this is Arpit Raj! Moving on keeping formalities aside- Empathy is a Leaders Strength. But that doesn't mean thay we cry in Layoffs, otherwise Board meetings would need Tissue Papers instead of Agendas."),
+    ]
+response = model.invoke(messages)
 print(response.content)
